4761756b46c013a8cee9ebc39eda805352f8b25d
jerrish <jers9791@gmail.com>
2023-01-22 16:52:00 +0530

    session6

--- 01_ec2_detailed.tf
+++ 01_ec2_detailed.tf
@@ -8,8 +8,15 @@ resource "aws_instance" "demo_instance" {
   ami                    = "ami-0cca134ec43cf708f"
   instance_type          = "t2.micro"
   key_name               = "terraformclass-mumbai-kp"
-  subnet_id              = "subnet-05f1ab6a25957a678"
-  vpc_security_group_ids = ["sg-079dc53d087d1f5a5"]
+  subnet_id              = aws_subnet.demo_subnet1.id
+  vpc_security_group_ids = [aws_security_group.demo_sg.id]
+}
+
+resource "time_sleep" "wait_a_min" {
+  create_duration = "1m"
+  depends_on = [
+    aws_instance.demo_instance
+  ]
 }
 
 /*
